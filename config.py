1854d05d3ce8d53cadfe699f2a69827d681c4b9d
Buse86 <98935332+Buse86@users.noreply.github.com>
2025-04-26 11:16:17 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+token = 'perm-YWRtaW4=.NDMtMA==.CHvmiOmIpZ2Ms8XnWMcdNy57X3hOnW'
+baseApi_url = 'https://dtk.youtrack.cloud/api/articles/'
+base_url = 'https://dtk.youtrack.cloud/articles/'
+
+api_key = 'AQVN0_82HCjAaxAB7lLHtQhB25duAcJP-RzBOpHP'
+folder_id = 'b1gbuumrugg2l8pe731q'
+
+headersAuth = {
+    'Authorization': 'Bearer ' + token
+}
